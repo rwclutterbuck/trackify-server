c1b8aa7401ebe442a3246dee754aada2da80b46e
k3ith99 <hamza_saleh1999@hotmail.com>
2022-02-21 14:25:18 +0000

    minor changes

--- api/models/habits.js
+++ api/models/habits.js
@@ -4,21 +4,14 @@ const { ObjectId } = require("mongodb")
 
 class Habits {
     constructor(data) {
-        this.id = data.id;
-        this.habit = data.habit;
-        this.habit.name = data.habit.name;
-        this.habit.frequency = data.habit.frequency;
-        this.habit.streak = data.habit.streak;
-        this.habit.history = data.habit.history;
-        this.habit.goal = data.habit.goal;
-        
+        this.name = data.name;
     }
     //list all habits of user and add id as a param
     static get allHabits(){
-        return new Promise(async(resolve,reject) => {
-            try{
+        return new Promise(async (resolve,reject) => {
+            try {
                 const db = await init();
-                const Userhabits2 = await db.collections("habits")
+                const Userhabits2 = await db.collection("habits").find().toArray()
                 // const Users_habits = User_habits.map(habit => new Habits({...habit}))
                 resolve(Userhabits2)
             }
--- db/seeds.js
+++ db/seeds.js
@@ -1,16 +1,20 @@
-db = connect("localhost:27017/habits")
+const db = connect("localhost:27017/habits")
 
-db.habits.insertOne([
+db.habits.drop();
+
+db.habits.insertOne(
     {
-        id: 1,
-        habit: [{
-            name: "water",
-            frequency: daily ,
-            streak: 1,
-            history: "monday",
-            goal: "do this for a year",
-            //lastLog: 
-        }]
- 
+        name: "water"
     }
-])
+    // {_id: 1, 
+    // habit: 
+    // {   
+    //     name: "water",
+    //     frequency: "daily" ,
+    //     streak: 1,
+    //     history: "monday",
+    //     goal: "do this for a year",
+    //     }
+ 
+    // }
+)
