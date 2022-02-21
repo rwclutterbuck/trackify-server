33306de0e18e22be73403e92b1297fc7f1301d0c
k3ith99 <hamza_saleh1999@hotmail.com>
2022-02-21 13:17:00 +0000

    created model to see all habits

--- api/models/habits.js
+++ api/models/habits.js
@@ -1,17 +1,31 @@
+const { init } = require("../initdb");
+const { ObjectId } = require("mongodb")
 
 
 class Habits {
     constructor(data) {
-        this.id = data.id,
+        this.id = data.id;
+        this.habit = data.habit;
         this.habit.name = data.habit.name;
         this.habit.frequency = data.habit.frequency;
         this.habit.streak = data.habit.streak;
         this.habit.history = data.habit.history;
         this.habit.goal = data.habit.goal;
+        
     }
-    //list all habits
-
-
+    //list all habits of user and add id as a param
+    static get allHabits(){
+        return new Promise(async(resolve,reject) => {
+            try{
+                const db = await init();
+                const Userhabits2 = await db.collections("habits")
+                // const Users_habits = User_habits.map(habit => new Habits({...habit}))
+                resolve(Userhabits2)
+            }
+            catch(err){
+                reject("Error retrieving User's habits")
+            }
+        })}
 
 
 
@@ -30,11 +44,16 @@ class Habits {
     //delete a habit
 
 
-    
+
 
 
     //delete everything
 
 
     
-}
+    }
+
+
+
+
+module.exports = Habits;
