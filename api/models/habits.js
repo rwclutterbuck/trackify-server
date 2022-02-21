723962c108254385a5b19cc8ccb15ab4458932d5
k3ith99 <hamza_saleh1999@hotmail.com>
2022-02-21 15:44:12 +0000

    docker compose working

--- api/models/habits.js
+++ api/models/habits.js
@@ -4,16 +4,21 @@ const { ObjectId } = require("mongodb")
 
 class Habits {
     constructor(data) {
-        this.name = data.name;
+        this.UserId = data.UserId;
+        this.habit = data.habit;
+        this.frequency = data.frequency;
+        this.streak = data.streak;
+        //this.habit.history = data.habit.history;
+        this.goal = data.goal;
     }
     //list all habits of user and add id as a param
     static get allHabits(){
         return new Promise(async (resolve,reject) => {
             try {
                 const db = await init();
-                const Userhabits2 = await db.collection("habits").find().toArray()
-                // const Users_habits = User_habits.map(habit => new Habits({...habit}))
-                resolve(Userhabits2)
+                const Userhabits = await db.collection("habits").find().toArray()
+                const Userhabits1 = Userhabits.map(habit => new Habits({...habit, id: habit._id}))
+                resolve(Userhabits1)
             }
             catch(err){
                 reject("Error retrieving User's habits")
