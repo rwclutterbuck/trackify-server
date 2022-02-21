329ff88da346113050f9fd357a23140c3a62b72e
rwclutterbuck <rwclutterbuck@outlook.com>
2022-02-21 17:31:56 +0000

    add: model Auth tests

---
+++ api/test/models/Auth.spec.js
@@ -0,0 +1,36 @@
+// Auth Model
+const Auth = require("../../models/Auth");
+const db = null; 
+
+// Methods to test
+describe("Auth", () => {
+  beforeEach(() => jest.clearAllMocks());
+  afterAll(() => jest.resetAllMocks());
+  // create
+  describe("create", () => {
+    it("creates new account", async () => {
+      const testUser = {
+        username: "mrMuscle",
+        password: "Password123",
+      };
+      jest.spyOn(db, "collection").mockResolvedValue()
+    });
+  });
+  // login
+  describe("login", () => {
+    it("verify user details", async () => {});
+  });
+  // changeEmail
+  describe("changeEmail", () => {
+    it("change user email", async () => {});
+  });
+
+  // changeUser
+  describe("changeUser", () => {
+    it("change username", async () => {});
+  });
+  // changePassword
+  describe("changePassword", () => {
+    it("change user password", async () => {});
+  });
+});
