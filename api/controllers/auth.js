215cd1a6704bfebb1b978eec1f4391a124490337
rwclutterbuck <rwclutterbuck@outlook.com>
2022-02-22 11:12:51 +0000

    fix: password assignment in controller

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -7,7 +7,7 @@ async function register(req, res) {
   try {
     const salt = await bcrypt.genSalt();
     const hashed = await bcrypt.hash(req.body.password, salt);
-    await User.create({ ...req.body, password: hashed });
+    await User.create({ ...req.body, password_hash: hashed });
     res.status(201).json({ msg: "User created" });
   } catch (err) {
     res.status(500).json({ err });
