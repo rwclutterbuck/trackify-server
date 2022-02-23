596cb0a564e225fce0841e922d23a99459a2909f
rwclutterbuck <rwclutterbuck@outlook.com>
2022-02-23 10:42:30 +0000

    add extra habit for user

--- db/seeds.js
+++ db/seeds.js
@@ -1,31 +1,36 @@
-const db = connect("localhost:27017/habits")
+const db = connect("localhost:27017/habits");
 
 db.habits.drop();
 
 db.habits.insertOne(
-    {
-        UserId: "1",
-        habit: "water",
-        frequency: "daily" ,
-        streak: 1,
-        //history: "monday",
-        goal: "do this for a year",
-    },
-    {
-        UserId: "2",
-        habit: "gym",
-        frequency: "daily",
-        streak: 2,
-        goal: "summer body"
-    },
-    {
-        UserId: "3",
-        habit: "diet",
-        frequency: "daily",
-        streak: 3,
-        goal: "till the summer",
-    }
-
-    
-)
+  {
+    UserId: "1",
+    habit: "water",
+    frequency: "daily",
+    streak: 1,
+    //history: "monday",
+    goal: "do this for a year",
+  },
+  {
+    UserId: "2",
+    habit: "gym",
+    frequency: "daily",
+    streak: 2,
+    goal: "summer body",
+  },
+  {
+    UserId: "3",
+    habit: "diet",
+    frequency: "daily",
+    streak: 3,
+    goal: "till the summer",
+  },
+  {
+    UserId: "1",
+    habit: "gym",
+    frequency: "daily",
+    streak: 2,
+    goal: "lose 5kg",
+  }
+);
 //add username
