e34a014ec42d8708caf71fff37ba8b588b5d8584
k3ith99 <hamza_saleh1999@hotmail.com>
2022-02-25 09:25:27 +0000

    added models and controller tests

--- api/tests/crud.test.js
+++ api/tests/crud.test.js
@@ -1,10 +1,19 @@
-const crudController = require('../controllers/Crud')
-const Habits = require('../models/habits')
-
+const crudController = require('../controllers/habits')
+const Habits = require('../models/Habit');
+const { init } = require('../server');
 const mockSend = jest.fn();
 const mockJson = jest.fn();
 const mockStatus = jest.fn(code => ({ send: mockSend, json: mockJson, end: jest.fn() }))
 const mockRes = { status: mockStatus }
+
+
+const testHabit = {
+    UserId: "10",
+    habit: "test",
+    frequency: "testing",
+    streak: 1,
+    goal: "for test purposes"
+}
 
 describe('habits controller', () => {
     beforeEach(() => jest.clearAllMocks());
@@ -17,19 +26,75 @@ describe('habits controller', () => {
                 .mockResolvedValue('deleted');
             
             const mockReq = { params: { id: 1 } }
-            await crudController.destroyOne(mockReq, mockRes)
+            await crudController.destroy(mockReq, mockRes)
             expect(mockStatus).toHaveBeenCalledWith(204);
         })
     })
 
-    describe('delete all', () => {
+    /*describe('delete all', () => {
         it('returns a 204 status code when successfully deleted', async () => {
             jest.spyOn(Habits.prototype, 'destroy')
-                .mockResolvedValue('deleted');
+                .mockResolvedValue('Deleted');
             
             const mockReq = { params: { id: 1 } }
-            await crudController.destroyAll(mockReq, mockRes)
+            await crudController.destroy(mockReq, mockRes)
             expect(mockStatus).toHaveBeenCalledWith(204);
+        })*/
+    })
+    describe("create a habit", () => {
+        it("returns new habit with a status code 201",
+        async () => {
+            jest.spyOn(Habits,"create")
+            .mockResolvedValue(expect.objectContaining(testHabit));
+         
+        const mockReq = { params: { UserId: 1 } }
+        await crudController.createHabit(mockReq,mockRes);
+        expect(mockStatus).toHaveBeenCalledWith(201);
+        expect(mockJson).toHaveBeenCalledWith(testHabit)
+        })
+  
+
+    })
+
+
+    
+    describe("show a habit", () =>{
+        it("shows habit with a status code of 200", async () => {
+            jest.spyOn(Habits, "showing one habit")
+            .mockResolvedValue(new Habits(testHabit));
+
+            const mockReq = { params:{UseId:1}, habit:"water"}
+            await crudController.specificHabit(mockReq, mockRes);
+            expect(mockStatus).toHaveBeenCalledWith(200);
         })
     })
-})
+  
+    describe("update streak", () => {
+        it("returns updated streak", async() => {
+            const updatedHabit = {
+                UserId: "10",
+                habit: "test",
+                frequency: "testing",
+                streak: 2,
+                goal: "for test purposes"
+            }
+            jest.spyOn(Habits, "update")
+            .mockResolvedValue(updatedHabit)
+            const mockReq = {params: {id:1}}
+            await crudController.updateStreak(mockReq,mockRes);
+            expect(mockStatus).toHaveBeenCalledWith(201);
+            expect(mockJson).toHaveBeenCalledWith(updatedHabit);
+
+        })
+    describe("show user habits", () => {
+        it("returns habits of user", async () => {
+            const mockReq = { params: { UserId : 1}}})
+        jest.spyOn("Habits", "show habits by UserId")
+        .mockResolvedValue(testHabit);
+        await crudController.specificHabit(mockReq, mockRes)
+        expect(mockJson).toHaveBeenCalledWith(testHabit)
+        expect(mockStatus).toHaveBeenCalledWith(200)
+    })    
+    })
+
+
--- api/tests/habits.test.js
+++ api/tests/habits.test.js
@@ -1,3 +1,88 @@
-const Habits = require("../models/habits")
+const Habits = require("../models/Habit")
+const { init } = require("../../api/initdb")
+jest.mock("mongodb")
+const db = require("../../")
 
-jest.mock()
+describe("Habit", () => {
+    let connection;
+    let db;
+
+    beforeAll(async () => {
+        connection = await init();
+        db = connection.db(process.env.DB_NAME);
+    });
+    afterAll(async () => {
+        await connection.close();
+    })
+    beforeEach(async () => {
+        await init();
+    })
+    
+    describe("findByUserId", () => {
+        it("finds data of userid after succesful query", async () => {
+            const habit1 = await Habits.findByUserId(1)
+            expect(habits).toEqual(objectContaining({
+                UserId: "1",
+                habit: "water",
+                frequency: "daily",
+                streak: 1,
+                //history: "monday",
+                goal: "do this for a year",
+            }))
+            })
+    describe("specificHabits", () => {
+        it("find specific habit of a user", async()=> {
+            const habit1 = await Habits.specificHabits(1,water)
+            expect(habit1).toBeInstanceOf(Habits)       
+        });
+    });
+    describe("create a habit", () => {
+        it("creates a new habit for an existing user", async () => {
+        const data ={
+            UserId: 10,
+            habit: "test", 
+            frequency: "daily",
+            streak: 1,
+            goal: "testing"}
+        const newHabit = await Habits.createHabit(data);
+        expect(newHabit).toEqual(objectContaining({
+            "UserId":10,
+            "habit":"test", 
+            "frequency":"daily",
+            "streak":1,
+            "goal":"testing"})
+        )
+        })
+
+    describe("update streak", () => {
+        it("updates streak in db", async() => {
+            const oldHabit = {
+            UserId: 10,
+            habit: "test", 
+            frequency: "daily",
+            streak: 1,
+            goal: "testing"
+            }
+            const updatedHabit = await Habits.updateStreak()
+            expect(updatedHabit).toEqual(objectContaining({
+            "UserId":10,
+            "habit":"test", 
+            "frequency":"daily",
+            "streak":2,
+            "goal":"testing"}))
+    describe("delete", () => {
+        it("deletes habit entry from db", async() => {
+            const data ={
+                UserId: 10,
+                habit: "test", 
+                frequency: "daily",
+                streak: 1,
+                goal: "testing"}
+            const deleted = await Habits.delete(data)
+            expect(deleted).toEqual(objectContaining({
+                //empty
+            }))
+        })
+    })
+    });
+    })
